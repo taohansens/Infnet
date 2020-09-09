a8ddf06dfe60c8cf9bdf7c7a873c936d800f5c3b
taohansens <tao.santos@somosicev.com>
2020-09-08 22:05:01 -0300

    Add

--- TP03/Q02_Eleicoes.py
+++ TP03/Q02_Eleicoes.py
@@ -1,11 +1,15 @@
+def verificaSituacaoEleitoral(idade):
+    if idade < 16:
+        print("Não tem direito a voto.")
+    elif (16 <= idade < 18) or (idade >= 70):
+        print("Não tem obrigação de votar.")
+    elif 18 <= idade < 70:
+        print("Tem obrigação de votar.")
+
 idade = 0
 while idade <= 0:
     idade = int(input("Informe a idade: "))
     if not (idade > 0):
         print(ValueError("Essa idade é inválida."))
-if idade < 16:
-    print("Não tem direito a voto.")
-elif (16 <= idade < 18) or (idade >= 70):
-    print("Não tem obrigação de votar.")
-elif 18 <= idade < 70:
-    print("Tem obrigação de votar.")
+
+verificaSituacaoEleitoral(idade)
