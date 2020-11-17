8bbecbf04e09e39f711d11cbbe573537a0a9691f
Tao Hansen <taohansens@gmail.com>
2020-11-16 23:49:40 -0300

    Q09 add [ok]

--- Bloco I/Fundamentos do Desenvolvimento Python/TP_03/Q09.py
+++ Bloco I/Fundamentos do Desenvolvimento Python/TP_03/Q09.py
@@ -1,5 +1,6 @@
 """
 Q.9 Usando a biblioteca Pygame, escreva um programa que possui uma função que desenha um círculo azul de 100 px de diâmetro no centro da tela. (código e printscreen)
+## PrintScreen armazenado em ./Q09 (circulo_azul).jpg
 """
 import pygame
 
@@ -7,10 +8,23 @@ pygame.init()
 largura_tela = 800
 altura_tela = 600
 
-clock = pygame.time.Clock()
 tela = pygame.display.set_mode((largura_tela, altura_tela))
 
+# Lista de cores
+branco = (255, 255, 255)
+azul = (0, 0, 200)
+
+
+# Função para desenhar círculo azul com raio 50px.
+def desenha_circulo_azul():
+    tela.fill(branco)
+    pygame.draw.circle(tela, azul, (largura_tela / 2, altura_tela / 2), 50)
+
+
 terminou = False
+# Chama função desenha circulo.
+desenha_circulo_azul()
+
 while not terminou:
     # atualiza a tela
     pygame.display.update()
@@ -19,13 +33,8 @@ while not terminou:
         if event.type == pygame.QUIT:
             terminou = True
 
-    # atualiza o desenho na tela
-    pygame.display.update()
-    # configura o clock em 60 atualizações.
-    clock.tick(60)
-    # finaliza a janela do jogo
+
+# finaliza a janela do jogo
 pygame.display.quit()
 # finaliza o pygame
 pygame.quit()
-
-
