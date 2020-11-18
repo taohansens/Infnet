6e963b63dd0543cdb4f7d91e9c401d118f755544
Tao Hansen <taohansens@gmail.com>
2020-11-18 14:27:09 -0300

    questões desafio

---
+++ Bloco I/Fundamentos do Desenvolvimento Python/TP_03/Q23.py
@@ -0,0 +1,5 @@
+"""
+Q.23
+(Desafio) Usando a biblioteca Pygame, escreva um programa que implemente o jogo da velha para dois jogadores
+(ambos usuários). (código e printscreen)
+"""
---
+++ Bloco I/Fundamentos do Desenvolvimento Python/TP_03/Q24.py
@@ -0,0 +1,13 @@
+"""
+Q.24
+(Desafio) Usando a biblioteca Pygame, escreva um programa que implemente o jogo da memória para um jogador.
+(código e printscreen)
+    O jogo da memória é um clássico jogo formado por peças que apresentam uma figura ou cor em um dos lados. Cada
+figura/cor se repete em duas peças diferentes. Para começar o jogo, as peças são postas com as figuras/cores voltadas
+para baixo, para que não possam ser vistas. Cada jogada do jogador consiste em virar duas peças, clicando nelas.
+    Caso as figuras/cores sejam iguais, as peças permanecem viradas. Se forem peças diferentes, estas devem ser viradas
+novamente. O jogo termina quando todas as peças forem viradas. A pontuação do jogador corresponde a todas as suas
+jogadas. Quanto menor o número de jogadas, melhor a sua pontuação.
+    As peças podem ser quadrados posicionadas em forma de matriz. Por exemplo, se forem 16 peças, elas podem ser
+    posicionadas na resolução 4x4 ou 2x8 ou como preferir.
+"""
