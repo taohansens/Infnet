abb8276f84a0d1749870eb95efcece16df8f1f88
Tao Hansen <taohansens@gmail.com>
2020-11-17 21:43:47 -0300

    Inicio

---
+++ Bloco I/Fundamentos do Desenvolvimento Python/TP_03/Q17.py
@@ -0,0 +1,4 @@
+"""
+Usando a biblioteca Pygame, escreva um programa que implemente o jogo “Pong” (visto no curso), com uma modificação.
+Tal modificação consiste em incluir o aumento da velocidade da bola. O aumento será feito de maneira gradual, isto é, cada 10 vezes que a bola bater na paleta do jogador1 a velocidade aumenta em 1. (código e printscreen)
+"""
