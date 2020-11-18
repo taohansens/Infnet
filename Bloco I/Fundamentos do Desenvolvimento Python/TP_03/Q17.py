05b75a26bc317295e2635b3ff391573187246931
Tao Hansen <taohansens@gmail.com>
2020-11-17 22:10:10 -0300

    Início

--- Bloco I/Fundamentos do Desenvolvimento Python/TP_03/Q17.py
+++ Bloco I/Fundamentos do Desenvolvimento Python/TP_03/Q17.py
@@ -2,3 +2,36 @@
 Usando a biblioteca Pygame, escreva um programa que implemente o jogo “Pong” (visto no curso), com uma modificação.
 Tal modificação consiste em incluir o aumento da velocidade da bola. O aumento será feito de maneira gradual, isto é, cada 10 vezes que a bola bater na paleta do jogador1 a velocidade aumenta em 1. (código e printscreen)
 """
+
+import pygame, sys
+from pygame.locals import *
+# CONSTANTES
+# FPS
+FPS = 60
+FPSCLOCK = pygame.time.Clock()
+
+# DISPLAY
+LARGURA_TELA = 800
+ALTURA_TELA = 600
+
+
+pygame.init()
+tela = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA))
+pygame.display.set_caption("Pong")
+
+terminou = False
+while not terminou:
+    # Atualiza o desenho na tela
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminou = True
+
+    FPSCLOCK.tick(FPS)
+
+# Finaliza a janela
+pygame.display.quit()
+# Finaliza o pygame
+pygame.quit()
+sys.exit()
