a0394c7f31ea465a2cdbccb7c526be8cec438989
Tao Hansen <taohansens@gmail.com>
2020-11-21 15:31:30 -0300

    Adicionadas imagens e função para desenhar o tabuleiro.

--- Bloco I/Fundamentos do Desenvolvimento Python/TP_03/Q23.py
+++ Bloco I/Fundamentos do Desenvolvimento Python/TP_03/Q23.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 # Inicializa pyGame
 pygame.init()
@@ -14,3 +15,24 @@ PRETO = (0, 0, 0)
 CINZA = (200, 200, 200)
 VERMELHO = (255, 0, 0)
 AZUL = (0, 0, 255)
+
+# Imagens
+X_IMAGE = pygame.transform.scale(pygame.image.load("Images/x.png"), (150, 150))
+O_IMAGE = pygame.transform.scale(pygame.image.load("Images/o.png"), (150, 150))
+
+# Fonte
+FONTE = pygame.font.SysFont('courier', 40)
+
+
+def desenha_tabuleiro():
+    x = 0
+    y = 0
+    # Calcula a posição para divisão da tela em 3 pontos.
+    distancia = SIZE_TELA // 3
+    for i in range(3):
+        x = i * distancia
+        # Desenha 3 linhas
+        pygame.draw.line(win, CINZA, (x, 0), (x, SIZE_TELA), 3)
+        # Desenha 3 colunas
+        pygame.draw.line(win, CINZA, (0, x), (SIZE_TELA, x), 3)
+
