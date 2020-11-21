30bd2ee859babe8224846d2e7634da932c8b5ddd
Tao Hansen <taohansens@gmail.com>
2020-11-20 22:37:43 -0300

    Iniciado código com POO (Problemas com a velocidade)

--- Bloco I/Fundamentos do Desenvolvimento Python/TP_03/Q17.py
+++ Bloco I/Fundamentos do Desenvolvimento Python/TP_03/Q17.py
@@ -1,6 +1,6 @@
 """
 PONG:
-Usando a biblioteca Pygame, escreva um programa que implemente o jogo “Pong” (visto no curso), com uma modificação.
+Q.17 Usando a biblioteca Pygame, escreva um programa que implemente o jogo “Pong” (visto no curso), com uma modificação.
 Tal modificação consiste em incluir o aumento da velocidade da bola. O aumento será feito de maneira gradual, isto é, cada 10 vezes que a bola bater na paleta do jogador1 a velocidade aumenta em 1. (código e printscreen)
 [x] + várias modificações.
 """
