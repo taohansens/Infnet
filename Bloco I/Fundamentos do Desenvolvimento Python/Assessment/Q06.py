c57e2a3f610b561bef14cf2d4b9e515becea4532
Tao Hansen <taohansens@gmail.com>
2020-11-30 07:41:02 -0300

    Q06 [ok] converted to function

--- Bloco I/Fundamentos do Desenvolvimento Python/Assessment/Q06.py
+++ Bloco I/Fundamentos do Desenvolvimento Python/Assessment/Q06.py
@@ -6,22 +6,26 @@ os números ímpares e uma nova tupla contendo somente os elementos nas posiçõ
 # tupla com valores
 tupla = (15, 10, 54, 66, 45, 8, 77, 53, 17, 11)
 
-# vetores vazios
-num_impares = []
-num_pos_pares = []
 
-for pos in range(len(tupla)):
-    # Verifica se o número é ímpar
-    if tupla[pos] % 2 != 0:
-        num_impares.append(tupla[pos])
-    # Verifica se o número está em uma posição par.
-    if pos % 2 == 0:
-        num_pos_pares.append(tupla[pos])
+def retorna_listas(tupla):
+    # vetores vazios
+    num_impares = []
+    num_pos_pares = []
 
-# Converte para tupla
-tuple_pos_pares = tuple(num_pos_pares)
+    for pos in range(len(tupla)):
+        # Verifica se o número é ímpar
+        if tupla[pos] % 2 != 0:
+            num_impares.append(tupla[pos])
+        # Verifica se o número está em uma posição par.
+        if pos % 2 == 0:
+            num_pos_pares.append(tupla[pos])
 
-# Lista com os números ímpares
-print(num_impares)
-# Tupla com os números das posições pares
-print(tuple_pos_pares)
+    # Converte para tupla
+    tuple_pos_pares = tuple(num_pos_pares)
+    return num_impares, tuple_pos_pares
+
+
+impares, pos_pares = retorna_listas(tupla)
+
+print("Números Ímpares: {} \nNum. nas posições pares: {}"
+      .format(impares, pos_pares))
