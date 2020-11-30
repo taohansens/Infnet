d837dde5ce5168c10369c57a04991037607dba58
Tao Hansen <taohansens@gmail.com>
2020-11-30 06:21:51 -0300

    Q01 Ok

---
+++ Bloco I/Fundamentos do Desenvolvimento Python/Assessment/Q01.py
@@ -0,0 +1,10 @@
+"""
+Q.1 Usando o Thonny, escreva um programa em Python que leia uma tupla contendo 3 números inteiros, (n1, n2, n3)
+e os imprima em ordem crescente.
+"""
+
+# Tupla contendo 3 números
+numeros = (10, 32, 4)
+
+# Mostrando em ordem crescente
+print(sorted(numeros))
