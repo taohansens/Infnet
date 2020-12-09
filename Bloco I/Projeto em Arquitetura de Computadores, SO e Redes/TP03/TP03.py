86d8e851f6fbd1f35d500bc5545698f952d198bf
Tao Hansen <taohansens@gmail.com>
2020-12-08 21:45:39 -0300

    Surface Resumo [Ok]

--- Bloco I/Projeto em Arquitetura de Computadores, SO e Redes/TP03/TP03.py
+++ Bloco I/Projeto em Arquitetura de Computadores, SO e Redes/TP03/TP03.py
@@ -46,7 +46,9 @@ ESPESSURA_BARRA = 14
 info_cpu = cpuinfo.get_cpu_info()
 
 
-# Processador
+# ************************************************************ #
+#                      Processador
+# ************************************************************ #
 def processador():
     uso_cpu = psutil.cpu_percent(percpu=True)
     surface_02 = pygame.Surface(TAM_TELA)
@@ -91,7 +93,9 @@ def processador():
         var_barra += 25
     TELA.blit(surface_02, (0, 0))
 
-
+# ************************************************************ #
+#                      Memória RAM
+# ************************************************************ #
 def memoria_ram():
     memoria = psutil.virtual_memory()
     surface_01 = pygame.Surface(TAM_TELA)
@@ -126,7 +130,9 @@ def memoria_ram():
     TELA.blit(surface_01, (0, 0))
 
 
-# Disco
+# ************************************************************ #
+#                      Disco
+# ************************************************************ #
 def disco():
     disk = verifica_discos()
     surface_03 = pygame.Surface(TAM_TELA)
@@ -167,7 +173,9 @@ def disco():
     TELA.blit(surface_03, (0, 0))
 
 
-# Rede
+# ************************************************************ #
+#                      Surface Rede
+# ************************************************************ #
 def rede(ip_pub):
     # Interface de rede terá que ser colocada manualmente, variação muito grande.
     INTERFACE_REDE = "Wi-Fi"
@@ -192,6 +200,9 @@ def rede(ip_pub):
     TELA.blit(surface_04, (0, 0))
 
 
+# ************************************************************ #
+#                      Surface Resumo
+# ************************************************************ #
 def resumo():
     surface_05 = pygame.Surface(TAM_TELA)
     surface_05.fill(CINZA)
@@ -201,7 +212,9 @@ def resumo():
     titulo = FONTE_TITLE.render("Resumo de Informações", True, ESCURO)
     surface_05.blit(titulo, (40, 20))
 
-    # Processador
+    # ************************************************************ #
+    #                      Processador
+    # ************************************************************ #
     titulo = FONTE_SUBINFO_BOLD.render("Processador", True, ESCURO)
     surface_05.blit(titulo, (40, 80))
     processor_name = "{}".format(info_cpu['brand_raw'])
@@ -235,6 +248,94 @@ def resumo():
     # Barra de uso (em vermelho)
     pos_final_barra_uso = 100 * (uso_cpu / 100)
     pygame.draw.rect(surface_05, VERMELHO, (430, 150, pos_final_barra_uso, ESPESSURA_BARRA))
+
+    # ************************************************************ #
+    #                      Memória RAM
+    # ************************************************************ #
+    memoria = psutil.virtual_memory()
+    titulo_memoria = FONTE_SUBINFO_BOLD.render("Memória", True, ESCURO)
+    surface_05.blit(titulo_memoria, (40, 210))
+    perc_texto = "{}%".format(memoria.percent)
+
+    percentagem_uso = FONTE_PERCENT.render(perc_texto, True, ESCURO)
+    surface_05.blit(percentagem_uso, (40, 230))
+
+    pos_altura_barra = 230
+    pos_final_barra = int(LARGURA_TELA - LARGURA_TELA * 0.2)
+    # Desenha barra total (em azul)
+    pygame.draw.rect(surface_05, AZUL, (100, pos_altura_barra, pos_final_barra, 20))
+    # Barra de uso (em vermelho)
+    pos_final_barra_uso = pos_final_barra * (memoria.percent / 100)
+    pygame.draw.rect(surface_05, VERMELHO, (100, pos_altura_barra, pos_final_barra_uso, 20))
+
+    memoria_total = "Memória física total: {:g} GB".format(round(memoria.total / (1024.0 ** 3), 0))
+    memoria_total_obj = FONTE_INFO.render(memoria_total, True, ESCURO)
+    surface_05.blit(memoria_total_obj, (pos_final_barra - 150, pos_altura_barra - 30))
+
+    # Memoria Física total
+    memoria_livre = "Memória em uso: {:.2f} GB | Memória livre: {:.2f} GB".format(memoria.used / (1024.0 ** 3),
+                                                                                  memoria.free / (1024.0 ** 3))
+    memoria_livre_obj = FONTE_INFO.render(memoria_livre, True, ESCURO)
+    surface_05.blit(memoria_livre_obj, (LARGURA_TELA // 5, pos_altura_barra + 20))
+
+    # ************************************************************ #
+    #                            Discos
+    # ************************************************************ #
+    disk = verifica_discos()
+    # Posições em pixels
+    pos_altura_barra = 320
+    pos_final_barra = int(LARGURA_TELA - LARGURA_TELA * 0.2)
+    # Desenha barra total (em azul)
+    pygame.draw.rect(surface_05, AZUL, (100, pos_altura_barra, pos_final_barra, 20))
+    # Barra de uso (em vermelho)
+    pos_final_barra_uso = pos_final_barra * (disk[5] / 100)
+    pygame.draw.rect(surface_05, VERMELHO, (100, pos_altura_barra, pos_final_barra_uso, 20))
+
+    # Início Textos
+    perc_texto = "{}%".format(disk[5])
+
+    titulo = FONTE_SUBINFO_BOLD.render("Espaço em Disco", True, ESCURO)
+    percentagem_uso = FONTE_PERCENT.render(perc_texto, True, ESCURO)
+    surface_05.blit(titulo, (40, 300))
+    surface_05.blit(percentagem_uso, (40, pos_altura_barra))
+
+    # Espaço total
+    qtd_total = "Encontrados: {:g}".format(round(disk[0]))
+    qtd_total_obj = FONTE_INFO.render(qtd_total, True, ESCURO)
+    mont_hds = "{}".format(disk[1])
+    mont_hds_obj = FONTE_INFO.render(mont_hds, True, ESCURO)
+    surface_05.blit(qtd_total_obj, (pos_final_barra - 50, pos_altura_barra + 20))
+    surface_05.blit(mont_hds_obj, (pos_final_barra - 40, pos_altura_barra + 40))
+
+    # Espaço livre/ocupado no hd
+    espaco_livre = "Espaço livre: {:.2f} GB".format(disk[4] / (1024.0 ** 3))
+    espaco_ocupado = "Espaço usado: {:.2f} GB".format(disk[3] / (1024.0 ** 3))
+    espaco_livre_obj = FONTE_INFO.render(espaco_livre, True, ESCURO)
+    espaco_ocupado_obj = FONTE_INFO.render(espaco_ocupado, True, ESCURO)
+    surface_05.blit(espaco_livre_obj, (70, pos_altura_barra + 20))
+    surface_05.blit(espaco_ocupado_obj, (70, pos_altura_barra + 40))
+
+    # ************************************************************ #
+    #                   Informações de REDE.
+    # ************************************************************ #
+    INTERFACE_REDE = "Wi-Fi"
+
+    dic_interfaces = psutil.net_if_addrs()
+    ip_rede = dic_interfaces[INTERFACE_REDE][1].address
+    net_mask = dic_interfaces[INTERFACE_REDE][1].netmask
+    ip_local = "IPv4 Local: {}".format(ip_rede)
+    ip_public = "IP Público: {}".format(ip_publico())
+    ip_netmask = "Máscara de Sub-rede: {}".format(net_mask)
+    ip_local_obj = FONTE_INFO.render(ip_local, True, ESCURO)
+    ip_netmask_obj = FONTE_INFO.render(ip_netmask, True, ESCURO)
+    ip_pub_obj = FONTE_INFO.render(ip_public, True, ESCURO)
+    titulo = FONTE_SUBINFO_BOLD.render("Informações de Rede", True, ESCURO)
+    surface_05.blit(titulo, (40, 400))
+    surface_05.blit(ip_local_obj, (40, 420))
+    surface_05.blit(ip_pub_obj, (40, 440))
+    surface_05.blit(ip_netmask_obj, (40, 460))
+    # ************************************************************ #
+
     TELA.blit(surface_05, (0,0))
 
 
@@ -325,6 +426,9 @@ def main():
                 if event.key == pygame.K_RIGHT:
                     if pagina < 5:
                             pagina += 1
+                if event.key == pygame.K_SPACE:
+                    pagina = 4
+
         if controle == 60:
             controle_setas()
             if pagina == 0:
