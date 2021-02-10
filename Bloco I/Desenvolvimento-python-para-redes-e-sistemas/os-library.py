063073b9ed8fc39b3cc81d4d4613364418beb807
Tao Hansen <taohansens@gmail.com>
2021-02-09 22:00:33 -0300

    timestamp GMT+3. Listando arquivos, com timestamp de access e modif.

--- Bloco I/Desenvolvimento-python-para-redes-e-sistemas/os-library.py
+++ Bloco I/Desenvolvimento-python-para-redes-e-sistemas/os-library.py
@@ -1,6 +1,9 @@
 # https://docs.python.org/3/library/os.html
-
 import os
+
+# Para timestamp
+from datetime import datetime
+from dateutil import tz
 
 # breakline
 br = '\n===================='
@@ -54,12 +57,47 @@ except FileNotFoundError:
     print("Repositório não disponível localmente")
 
 for arquivo in lista_arquivos:
+    # determinado formato
     if '.gif' in arquivo:
         print(arquivo)
-
+print(br)
 
 # Verificar se é arquivo ou pasta:
-# os.path.isfile()
+print(os.path.isfile('os-library.py'), br)
+
+# Os.STAT
+print(os.stat('os-library.py'), br)
+# apenas tamanho do arquivo em KB
+print(f"{os.stat('os-library.py').st_size//1024} KB")
+
+# timestamp em formato de data utilizando datetime
+print(datetime.fromtimestamp(os.stat('os-library.py').st_mtime, tz=tz.tzlocal())
+      .strftime('%d/%m/%Y-%H:%M'), br)
+
+
+# função para timestamp
+def timestamp(file):
+    atime = datetime.fromtimestamp(os.stat(file).st_atime, tz=tz.tzlocal()).strftime('%d/%m/%Y-%H:%M')
+    mtime = datetime.fromtimestamp(os.stat(file).st_mtime, tz=tz.tzlocal()).strftime('%d/%m/%Y-%H:%M')
+    return atime, mtime
+
+
+# Imprimindo lista de arquivos da pasta downloads
+my_folder = '../../../../Downloads'
+l_arquivos = os.listdir(my_folder)
+if os.path.exists(my_folder):
+    for arquivo in l_arquivos:
+        caminho_arquivo = os.path.join(my_folder, arquivo)
+        if os.path.isfile(caminho_arquivo):
+            arquivo_stat = os.stat(caminho_arquivo)
+            time = timestamp(caminho_arquivo)
+            #print(arquivo, f"{arquivo_stat.st_size//1024}KB", arquivo_stat.st_uid, time[0], time[1])
+            print('{0: <75}'.format(arquivo), "|", '{0: <20}'.format(time[0]), "|",'{0: <20}'.format(time[1]))
+
+
+
+
+
 
 
 
