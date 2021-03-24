1ef6efd6ca279300850b03a2d42465b06fc7523a
taohansens <taohansens@gmail.com>
2021-03-24 20:45:01 -0300

    TestServer

--- Bloco I/Projeto em Arquitetura de Computadores, SO e Redes/TP09/Client_T`P09.py
+++ Bloco I/Projeto em Arquitetura de Computadores, SO e Redes/TP09/Client_T`P09.py
@@ -1,12 +1,32 @@
+import pickle
 import socket
 
-host = socket.gethostname()
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# host = socket.gethostname()
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+# try:
+#     s.connect((host, 5000))
+# except ConnectionRefusedError as error:
+#     print(error)
 
-try:
-    s.connect((host, 5000))
-except ConnectionRefusedError as error:
-    print(error)
 
-s.send(b"Inicio")
-s.close()
+def get_server(solicitacao):
+    host = socket.gethostname()
+    porta = 5000
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((host, porta))
+        s.send(f"{solicitacao}".encode('ascii'))
+        dados = pickle.loads(s.recv(10000))
+        s.close()
+        informacoes = [dados, (host, porta)]
+    except ConnectionRefusedError as error:
+        dados = {'conexao': 'ERROR'}
+        informacoes = [dados, (host, porta)]
+        print(error)
+    return informacoes
+
+
+print(get_server("REDE"))
+
+# s.close()
