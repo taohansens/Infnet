51296b4b01b743a5c9f8a8456e086db3d790ab56
taohansens <taohansens@gmail.com>
2021-04-05 08:40:12 -0300

    Q05 ok

---
+++ Bloco I/Desenvolvimento-python-para-redes-e-sistemas/AT/Q05.py
@@ -0,0 +1,28 @@
+
+def transforma_arquivos(arquivo1, arquivo2):
+    textfile1 = open(arquivo1, "r", encoding="utf-8")
+    numeros1 = textfile1.read().split(' ')
+    textfile2 = open(arquivo2, "r", encoding="utf-8")
+    numeros2 = textfile2.read().split(' ')
+    print(len(numeros1), len(numeros2))
+    if len(numeros1)<len(numeros2):
+        qtd = len(numeros2) - len(numeros1)
+        for i in range(qtd):
+            numeros1.append(0)
+    if len(numeros2)<len(numeros1):
+        qtd = len(numeros1) - len(numeros2)
+        for i in range(qtd):
+            numeros2.append(0)
+    return numeros1, numeros2
+
+
+def main():
+    file_one = "q05a.txt"
+    file_two = "q05b.txt"
+    lista = transforma_arquivos(file_one, file_two)
+    soma = [int(lista[0][i]) + int(lista[1][i]) for i in range(len(lista[0]))]
+    print(soma)
+
+
+if __name__ == "__main__":
+    main()
