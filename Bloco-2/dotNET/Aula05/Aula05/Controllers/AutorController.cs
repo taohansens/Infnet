d00140d78fb1f0718853da7f5fa2e3ebc9c0590f
taohansens <taohansens@gmail.com>
2021-05-24 22:15:53 -0300

    Aula 05. First MVC. AutorView.

---
+++ Bloco-2/dotNET/Aula05/Aula05/Controllers/AutorController.cs
@@ -0,0 +1,22 @@
+﻿using Aula05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula05.Controllers {
+    public class AutorController : Controller {
+        public IActionResult Index() {
+            var autorModel = new AutorModel();
+            autorModel.Id = 1;
+            autorModel.Nome = "Tao Hansen";
+
+            return View(autorModel);
+        }
+
+        public IActionResult Index2() {
+            var autorModel = new AutorModel();
+            autorModel.Id = 1;
+            autorModel.Nome = "Tao Hansen";
+
+            return View(autorModel);
+        }
+    }
+}
